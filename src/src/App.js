7f9318ef48604915fe632a1ea1a262e6a637dc10
houmaiwakiri <houmaiwakiri@users.noreply.github.com>
2021-09-22 21:07:14 +0900

    一時中断してreact勉強、タイトルとボタンだけ表示できた

--- src/src/App.js
+++ src/src/App.js
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
 
 function App() {
   return (
       <div class="header">
         <h1 class="title">となりプロジェクト</h1>
-        <Button variant="contained" color="primary">日本地図</Button>
+        <button　class="menu">日本地図</button>
       </div>
   );
 }
 
-ReactDOM.render(<App />, document.querySelector('#app'));
 
 export default App;
