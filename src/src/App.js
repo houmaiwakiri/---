8c912ea3caf69b374483e049cf3901829eb14c40
houmaiwakiri <houmaiwakiri@users.noreply.github.com>
2021-09-22 20:55:34 +0900

    give up

--- src/src/App.js
+++ src/src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-    <div className="App">
       <div class="header">
-        <h1>となりプロジェクト</h1>
+        <h1 class="title">となりプロジェクト</h1>
+        <Button variant="contained" color="primary">日本地図</Button>
       </div>
-      <div>
-        <h2>日本地図</h2>
-      </div>
-    </div>
   );
 }
+
+ReactDOM.render(<App />, document.querySelector('#app'));
 
 export default App;
